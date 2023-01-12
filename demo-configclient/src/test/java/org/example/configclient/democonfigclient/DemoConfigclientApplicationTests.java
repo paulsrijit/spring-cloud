820c204f23af7e5ea6e96a7176d99bc110fccb4a
Srijit Paul <paul.srijit@gmail.com>
2023-01-12 17:12:24 +0530

    Initial commit

---
+++ demo-configclient/src/test/java/org/example/configclient/democonfigclient/DemoConfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.configclient.democonfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ demo-configserver/src/test/java/org/example/configserver/democonfigserver/DemoConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.configserver.democonfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
